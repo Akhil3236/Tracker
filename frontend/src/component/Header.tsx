b02e3954f4ff051e630baca6b349c3a2205a485a
akhil <akhiltuluri123@gmail.com>
2025-08-23 18:44:23 +0530

    layout

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 export default function Header() {
     return (
       <header style={{ padding: "1rem", background: "#222", color: "#fff" }}>
+        
         <nav>
             <Link href="/">Home</Link>
-            <Link href="/about">about me</Link>
+            <Link href="/about">abou me</Link>
         </nav>
       </header>
-);
-}
+    );
+  }
   
