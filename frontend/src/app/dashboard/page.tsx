2c586937fb6a00dff688d7a270990791695008af
akhil <akhiltuluri123@gmail.com>
2025-08-25 20:10:16 +0530

    added the authentication by passing cookie

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import axios from 'axios';
 
 type Props = {}
 
 function page({}: Props) {
+
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+    const [userdata,setuserdata]=useState({});
+  
+    useEffect(() => {
+        const checkAuth = async () => {
+          try {
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}user`, {
+              withCredentials: true, 
+            });
+            
+            setLoading(false);
+            setuserdata(res.data);
+          } catch (err) {
+            console.error("Not authenticated", err);
+            router.push("/login"); 
+          }
+        };
+    
+        checkAuth();
+      });
+
+      if (loading) return <p>Loading...</p>;
   return (
     <>
     
     <div className="main">
 
+        
         DashBoard 
 
         <br />
-       <button>refresh</button>
+       <button>Authroized</button>
     </div>
      </>
   )
