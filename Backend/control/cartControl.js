038470b814ce9539f3ae9da783cfcfd9f0292b18
akhil <akhiltuluri123@gmail.com>
2025-09-12 20:08:01 +0530

    adeed the order serevrs and set up the sqs and from sqs lamnda is setup in aws next -> has to make the lamanda configguration for it

--- Backend/control/cartControl.js
+++ Backend/control/cartControl.js
@@ -2,8 +2,15 @@ import { Cart } from "../models/cart/cart.js"
 import { Order } from "../models/order/order.js";
 import { Products } from "../models/products/product.js"
 import dotenv from "dotenv"
-
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 dotenv.config();
+const sqsClient = new SQSClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+  },
+});
 /*---------------------------------
 1) add the product in the cart
 ---------------------------------*/
@@ -182,7 +189,7 @@ export const order = async (req, res) => {
     });
 
 
-    await Cart.findOneAndDelete({ userId });
+
 
     const params = {
       QueueUrl: process.env.SQS_QUEUE_URL,
@@ -191,18 +198,18 @@ export const order = async (req, res) => {
         userId: userId,
         items: populatedItems,
         totalPrice: totalPrice
-      })
+      }),
+      MaxNumberOfMessages: 5,
+      WaitTimeSeconds: 10
     };
 
-    console.log(params);
-
-    //  await sqs.sendMessage(params).promise();
-
+    await sqsClient.send(new SendMessageCommand(params));
+    
     return res.status(200).json({
       message: 'Order placed and queued for processing',
       orderId: newOrder._id
     });
-   
+
   } catch (error) {
     console.error(error);
     res.status(500).json({
