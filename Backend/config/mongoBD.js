7d7a267e56e1c290df08b6b0560a2bca5d2c0e0a
Harsha <harshaalapati1324@gmail.com>
2025-09-13 14:59:42 +0530

    setup change

--- Backend/config/mongoBD.js
+++ Backend/config/mongoBD.js
@@ -4,9 +4,9 @@ import dns from 'dns';
 
 export const connectDB = async () => {
     
-    try {
-        dns.setServers(['8.8.8.8', '1.1.1.1']);
-    } catch {}
+    // try {
+    //     dns.setServers(['8.8.8.8', '1.1.1.1']);
+    // } catch {}
 
     await mongoose.connect(`${process.env.MONGODB_URL}`);
     mongoose.connection.on('connected', () => console.log('Database Connected'));
